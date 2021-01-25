e4a672069e2a0dd61e4cf0d71f0b8f41a93ccf5f
chase_jiang <xjiang@colgate.edu>
2021-01-25 13:21:59 -0500

    relation fixed

--- latency/extraction_bird.py
+++ latency/extraction_bird.py
@@ -63,7 +63,15 @@ def trial_1(fname):
                 elif des_router == "10.10.0.3":
                     r2.append(message+"Receive at "+time + " from " +src_router)
                 elif des_router == "10.10.0.11":
-                    r3.append(message+"Receive at "+time + " from " +src_router)
+                    r3.append(message+"Receive at "+time + " from " +src_router)\
+
+    r1_send_if_recv =defaultdict(set)
+    r1_recv_if_send =defaultdict(set)
+    r2_send_if_recv =defaultdict(set)
+    r2_recv_if_send =defaultdict(set)
+    r3_send_if_recv =defaultdict(set)
+    r3_recv_if_send =defaultdict(set)
+
     r1_send = []
     r1_receive = []
     for item in reversed(r1):
@@ -71,35 +79,25 @@ def trial_1(fname):
             r1_send.append(item)
         else:
             r1_receive.append(item)
-    r1_send2 = r1_send
-    r1_receive2 = r1_receive
-    r1_send_if_recv =defaultdict(set)
-    r1_recv_if_send =defaultdict(set)
-
     for send_item in r1_send:
         send_result = re.search('Send at (.*) to ', send_item)
         send_time = float(send_result.group(1))
+        send_to_result = re.search('to (.*)', send_item)
+        send_to = str(send_to_result.group(1))
         for recv_item in r1_receive:
             recv_result = re.search('Receive at (.*) from ', recv_item)
             recv_time = float(recv_result.group(1))
-            if (recv_time - send_time)>6:
+            recv_from_result = re.search('from (.*)', recv_item)
+            recv_from = str(recv_from_result.group(1))
+            if (recv_time - send_time)>6 and send_to == recv_from:
                 send_packet = re.search('(.*)Send at', send_item)
                 recv_packet = re.search('(.*)Receive at', recv_item)
                 r1_receive.remove(recv_item)
+                if send_to == "10.10.0.3":
+                    r2_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
+                elif send_to == "10.10.0.11":
+                    r3_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 r1_recv_if_send[send_packet.group(1)].add(recv_packet.group(1))
-                break
-
-    for recv_item in r1_receive2:
-        recv_result = re.search('Receive at (.*) from ', recv_item)
-        recv_time = float(recv_result.group(1))
-        for send_item in r1_send2:
-            send_result = re.search('Send at (.*) to ', send_item)
-            send_time = float(send_result.group(1))
-            if (send_time - recv_time)>6:
-                recv_packet = re.search('(.*)Receive at', recv_item)
-                send_packet = re.search('(.*)Send at', send_item)
-                r1_send.remove(send_item)
-                r1_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 break
 
 
@@ -112,35 +110,26 @@ def trial_1(fname):
             r2_send.append(item)
         else:
             r2_receive.append(item)
-    r2_send2 = r2_send
-    r2_receive2 = r2_receive
-    r2_send_if_recv =defaultdict(set)
-    r2_recv_if_send =defaultdict(set)
-
+            
     for send_item in r2_send:
         send_result = re.search('Send at (.*) to ', send_item)
         send_time = float(send_result.group(1))
+        send_to_result = re.search('to (.*)', send_item)
+        send_to = str(send_to_result.group(1))
         for recv_item in r2_receive:
             recv_result = re.search('Receive at (.*) from ', recv_item)
             recv_time = float(recv_result.group(1))
-            if (recv_time - send_time)>6:
+            recv_from_result = re.search('from (.*)', recv_item)
+            recv_from = str(recv_from_result.group(1))
+            if (recv_time - send_time)>6 and send_to == recv_from:
                 send_packet = re.search('(.*)Send at', send_item)
                 recv_packet = re.search('(.*)Receive at', recv_item)
                 r2_receive.remove(recv_item)
+                if send_to == "10.10.0.2":
+                    r1_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
+                elif send_to == "10.10.0.11":
+                    r3_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 r2_recv_if_send[send_packet.group(1)].add(recv_packet.group(1))
-                break
-
-    for recv_item in r2_receive2:
-        recv_result = re.search('Receive at (.*) from ', recv_item)
-        recv_time = float(recv_result.group(1))
-        for send_item in r2_send2:
-            send_result = re.search('Send at (.*) to ', send_item)
-            send_time = float(send_result.group(1))
-            if (send_time - recv_time)>6:
-                recv_packet = re.search('(.*)Receive at', recv_item)
-                send_packet = re.search('(.*)Send at', send_item)
-                r2_send.remove(send_item)
-                r2_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 break
 
 #############################################################################################################################
@@ -152,35 +141,26 @@ def trial_1(fname):
             r3_send.append(item)
         else:
             r3_receive.append(item)
-    r3_send2 = r3_send
-    r3_receive2 = r3_receive
-    r3_send_if_recv =defaultdict(set)
-    r3_recv_if_send =defaultdict(set)
 
     for send_item in r3_send:
         send_result = re.search('Send at (.*) to ', send_item)
         send_time = float(send_result.group(1))
+        send_to_result = re.search('to (.*)', send_item)
+        send_to = str(send_to_result.group(1))
         for recv_item in r3_receive:
             recv_result = re.search('Receive at (.*) from ', recv_item)
             recv_time = float(recv_result.group(1))
-            if (recv_time - send_time)>6:
+            recv_from_result = re.search('from (.*)', recv_item)
+            recv_from = str(recv_from_result.group(1))
+            if (recv_time - send_time)>6 and send_to == recv_from:
                 send_packet = re.search('(.*)Send at', send_item)
                 recv_packet = re.search('(.*)Receive at', recv_item)
                 r3_receive.remove(recv_item)
+                if send_to == "10.10.0.2":
+                    r1_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
+                elif send_to == "10.10.0.3":
+                    r2_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 r3_recv_if_send[send_packet.group(1)].add(recv_packet.group(1))
-                break
-
-    for recv_item in r3_receive2:
-        recv_result = re.search('Receive at (.*) from ', recv_item)
-        recv_time = float(recv_result.group(1))
-        for send_item in r3_send2:
-            send_result = re.search('Send at (.*) to ', send_item)
-            send_time = float(send_result.group(1))
-            if (send_time - recv_time)>6:
-                recv_packet = re.search('(.*)Receive at', recv_item)
-                send_packet = re.search('(.*)Send at', send_item)
-                r3_send.remove(send_item)
-                r3_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 break
 
     return r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_recv_if_send, r3_send_if_recv
@@ -226,6 +206,10 @@ def trial_2(fname):
                     r1.append(message+"Receive at "+time + " from " +src_router)
                 elif des_router == "10.10.0.3":
                     r2.append(message+"Receive at "+time + " from " +src_router)
+    r1_send_if_recv =defaultdict(set)
+    r1_recv_if_send =defaultdict(set)
+    r2_send_if_recv =defaultdict(set)
+    r2_recv_if_send =defaultdict(set)
 
     r1_send = []
     r1_receive = []
@@ -234,39 +218,24 @@ def trial_2(fname):
             r1_send.append(item)
         else:
             r1_receive.append(item)
-    #for item in r1_send:
-    #    print(item)
-    #for item in r1_receive:
-    #    print(item)
-    r1_send2 = r1_send
-    r1_receive2 = r1_receive
-    r1_send_if_recv =defaultdict(set)
-    r1_recv_if_send =defaultdict(set)
 
     for send_item in r1_send:
         send_result = re.search('Send at (.*) to ', send_item)
         send_time = float(send_result.group(1))
+        send_to_result = re.search('to (.*)', send_item)
+        send_to = str(send_to_result.group(1))
         for recv_item in r1_receive:
             recv_result = re.search('Receive at (.*) from ', recv_item)
             recv_time = float(recv_result.group(1))
-            if (recv_time - send_time)>6:
+            recv_from_result = re.search('from (.*)', recv_item)
+            recv_from = str(recv_from_result.group(1))
+            if (recv_time - send_time)>6 and send_to == recv_from:
                 send_packet = re.search('(.*)Send at', send_item)
                 recv_packet = re.search('(.*)Receive at', recv_item)
                 r1_receive.remove(recv_item)
+                if send_to == "10.10.0.3":
+                    r2_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 r1_recv_if_send[send_packet.group(1)].add(recv_packet.group(1))
-                break
-
-    for recv_item in r1_receive2:
-        recv_result = re.search('Receive at (.*) from ', recv_item)
-        recv_time = float(recv_result.group(1))
-        for send_item in r1_send2:
-            send_result = re.search('Send at (.*) to ', send_item)
-            send_time = float(send_result.group(1))
-            if (send_time - recv_time)>6:
-                recv_packet = re.search('(.*)Receive at', recv_item)
-                send_packet = re.search('(.*)Send at', send_item)
-                r1_send.remove(send_item)
-                r1_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 break
 
 
@@ -279,35 +248,24 @@ def trial_2(fname):
             r2_send.append(item)
         else:
             r2_receive.append(item)
-    r2_send2 = r2_send
-    r2_receive2 = r2_receive
-    r2_send_if_recv =defaultdict(set)
-    r2_recv_if_send =defaultdict(set)
 
     for send_item in r2_send:
         send_result = re.search('Send at (.*) to ', send_item)
         send_time = float(send_result.group(1))
+        send_to_result = re.search('to (.*)', send_item)
+        send_to = str(send_to_result.group(1))
         for recv_item in r2_receive:
             recv_result = re.search('Receive at (.*) from ', recv_item)
             recv_time = float(recv_result.group(1))
-            if (recv_time - send_time)>6:
+            recv_from_result = re.search('from (.*)', recv_item)
+            recv_from = str(recv_from_result.group(1))
+            if (recv_time - send_time)>6 and send_to == recv_from:
                 send_packet = re.search('(.*)Send at', send_item)
                 recv_packet = re.search('(.*)Receive at', recv_item)
                 r2_receive.remove(recv_item)
+                if send_to == "10.10.0.2":
+                    r1_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 r2_recv_if_send[send_packet.group(1)].add(recv_packet.group(1))
-                break
-
-    for recv_item in r2_receive2:
-        recv_result = re.search('Receive at (.*) from ', recv_item)
-        recv_time = float(recv_result.group(1))
-        for send_item in r2_send2:
-            send_result = re.search('Send at (.*) to ', send_item)
-            send_time = float(send_result.group(1))
-            if (send_time - recv_time)>6:
-                recv_packet = re.search('(.*)Receive at', recv_item)
-                send_packet = re.search('(.*)Send at', send_item)
-                r2_send.remove(send_item)
-                r2_send_if_recv[recv_packet.group(1)].add(send_packet.group(1))
                 break
 
 
@@ -363,11 +321,11 @@ def run(r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_r
 
 
 def main():
-    #r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_recv_if_send, r3_send_if_recv = trial_1('lb800_1_3.txt')
-    #r4_recv_if_send, r4_send_if_recv, r5_recv_if_send, r5_send_if_recv, r6_recv_if_send, r6_send_if_recv = trial_1('lb800_2_3.txt')
-    #r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv = trial_2('lb1000_1_2.txt')
-    #run(r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_recv_if_send, r3_send_if_recv, r4_recv_if_send, r4_send_if_recv, r5_recv_if_send, r5_send_if_recv, r6_recv_if_send, r6_send_if_recv, r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv)
-    r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv = trial_2('example.txt')
+    r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_recv_if_send, r3_send_if_recv = trial_1('lb800_1_3.txt')
+    r4_recv_if_send, r4_send_if_recv, r5_recv_if_send, r5_send_if_recv, r6_recv_if_send, r6_send_if_recv = trial_1('lb800_2_3.txt')
+    r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv = trial_2('lb1000_1_2.txt')
+    run(r1_recv_if_send, r1_send_if_recv, r2_recv_if_send, r2_send_if_recv, r3_recv_if_send, r3_send_if_recv, r4_recv_if_send, r4_send_if_recv, r5_recv_if_send, r5_send_if_recv, r6_recv_if_send, r6_send_if_recv, r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv)
+    #r7_recv_if_send, r7_send_if_recv, r8_recv_if_send, r8_send_if_recv = trial_2('example.txt')
 
 main()
 #DB Description (2)
